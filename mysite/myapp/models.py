114fb122c9e133be09f41ed219687a566c0bfa96
LucasGolabek <luka80471@gmail.com>
2022-11-09 16:55:41 +0100

    added offer rendering, rest of templates and starting modeling database

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField()
+    description = models.CharField(max_length=600, null=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+
--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.mainpage, name='mainpage'),
-    path('buy/', views.cart, name='buy'),
-    path('checkout/', views.checkout, name='checkout'),
+    path('buy/', views.buy, name='buy'),
     path('product/', views.product, name='product'),
     path('login/', views.login_page, name='login'),
-    path('register/', views.register_page, name='register')
+    path('logout/', views.logout_page, name='logout'),
+    path('active/', views.active, name='active'),
+    path('crate/', views.create, name='create'),
+    path('messages/', views.messages, name='messages')
 ]
