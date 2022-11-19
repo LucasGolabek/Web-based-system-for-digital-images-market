32f9a40eb508da6e28ab7ddc22077c76702cbd35
LucasGolabek <luka80471@gmail.com>
2022-11-19 12:57:12 +0100

    added database tables, creating forms

--- mysite/myapp/forms.py
+++ mysite/myapp/forms.py
@@ -17,3 +17,13 @@ class CreateUserForm(UserCreationForm):
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
+
+
+class AddImageForm():
+    usage_possibilities = (('P', 'Prywatne'), ('K', 'Komercyjne'), ('KO', 'Komercyjne ograniczone'))
+
+    photo_name = forms.CharField(label='Nazwa zdjęcia', max_length=50)
+    username = forms.CharField(label='Twoja nazwa użytkownika', max_length=100)
+    description = forms.CharField(label='Opis zdjęcia', max_length=600)
+    price = forms.FloatField()
+    usage = forms.CharField(max_length=2, choices=usage_possibilities)
--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -2,13 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+
 # Create your models here.
 
 class Product(models.Model):
+    usage_possibilities = (('P', 'Prywatne'), ('K', 'Komercyjne'), ('KO', 'Komercyjne ograniczone'))
+
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
-    description = models.CharField(max_length=600, null=True)
+    description = models.CharField(max_length=600, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    username = models.CharField(max_length=100, null=True)
+    usage = models.CharField(max_length=2, choices=usage_possibilities)
 
     def __str__(self):
         return self.name
@@ -22,4 +27,14 @@ class Product(models.Model):
         return url
 
 
+class Messages(models.Model):
+    usage_possibilities = (('P', 'Prywatne'), ('K', 'Komercyjne'), ('KO', 'Komercyjne ograniczone'))
+    status_possibilities = (('A', 'Zaakceptowana'), ('O', 'Odrzucona'))
 
+    photo_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user_from = models.CharField(max_length=100)
+    user_to = models.CharField(max_length=100)
+    message_text = models.CharField(max_length=600, null=True, blank=True)
+    negotiation_price = models.FloatField()
+    negotiation_usage = models.CharField(max_length=2, choices=usage_possibilities)
+    negotiation_status = models.CharField(max_length=1, choices=status_possibilities, blank=True)
